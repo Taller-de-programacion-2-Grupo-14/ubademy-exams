8923654717aebd83cfba201ea1451a0e6c992974
Ignacio Carol Lugones <icarol@fi.uba.ar>
2021-12-14 01:46:45 -0300

    add persistence side of exams

--- persistence/mongo.py
+++ persistence/mongo.py
@@ -1,31 +1,66 @@
-import os
+def _getExamKey(name, courseId):
+    return {'title': name, 'course': courseId}
 
-import pymongo
 
+def _getKeyResolution(name, userId, courseId):
+    key = {}
+    if name:
+        key.update({'exam': name})
+    if userId:
+        key.update({'user': userId})
+    if courseId:
+        key.update({'course': courseId})
+    return key
 
 
 class MongoDB:
     def __init__(self, db):
         self.db = db
 
-    def saveExam(self, item: dict):
-        key = {'id': item.get('id')}
-        response = self.db.exams.update_one(key, {'$set': item}, upsert=True)
+    def createExam(self, name: str, courseId, questions: dict):
+        key = _getExamKey(name, courseId)
+        item = {'status': 'drafted'}
+        item.update({'questions': questions})
+        response = self.db.exams.update_one(key, {'$set': questions}, upsert=True)
         return response.acknowledge
 
-    def getExam(self, item: dict):
-        return self.db.exams.find_one(item)
-
-    def getExams(self, item: dict):
-        return list(self.db.exams.find(item))
-
-    def addResponse(self, item: dict):
-        key = {'id': f'{item.get("user_id")}-{item.get("id")}'}
-        response = self.db.responses.update_one(key, {'$set': item}, upsert=True)
+    def editExam(self, courseId, name, questions):
+        key = _getExamKey(name, courseId)
+        response = self.db.exams.update_one(key, {'$set': {'questions': questions}}, upsert=True)
         return response.acknowledge
 
-    def getResponse(self, item: dict):
-        return self.db.responses.find_one(item, upsert=True)
+    def getExam(self, name, courseId):
+        return self.db.exams.find_one(_getExamKey(name, courseId))
 
-    def getResponses(self, item: dict):
+    def getExams(self, name, courseId):
+        item = {}
+        if name:
+            item.update({'name': name})
+        if courseId:
+            item.update({'course': courseId})
         return list(self.db.exams.find(item))
+
+    def publishExam(self, name, courseId):
+        key = _getExamKey(name, courseId)
+        response = self.db.exams.update_one(key, {'$set': {'status': 'published'}}, upsert=True)
+        return response.acknowledge
+
+    def addResolution(self, userId, examName, courseId, item: dict):
+        key = _getKeyResolution(examName, userId, courseId)
+        response = self.db.responses.update_one(key, {'$set': {'answer': item}}, upsert=True)
+        return response.acknowledge
+
+    def getResolution(self, name, userId, courseId):
+        key = _getKeyResolution(name, userId, courseId)
+        return self.db.responses.find_one(key, upsert=True)
+
+    def getResponses(self, name, userId, courseId):
+        key = _getKeyResolution(name, userId, courseId)
+        return list(self.db.exams.find(key))
+
+    def gradeExam(self, name, userId, courseId, corrections: dict, status):
+        if not (name and userId and courseId):
+            raise Exception
+        correction = {'correction': corrections, 'status': status}
+        response = self.db.responses.update_one(_getKeyResolution(name, userId, courseId), {'$set': correction})
+        return response.acknowledge
