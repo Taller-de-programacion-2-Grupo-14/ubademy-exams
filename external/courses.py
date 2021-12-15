14cc803de659e18b1a42a93317a9a1a4c495dd8e
Ignacio Carol Lugones <icarol@fi.uba.ar>
2021-12-15 00:20:11 -0300

    add code for exams on mongo

--- external/courses.py
+++ external/courses.py
@@ -4,10 +4,10 @@ import requests
 
 class Courses:
     def __init__(self):
-        self.host = os.environ.get("USERS_HOSTNAME")
+        self.host = os.environ.get("COURSES_HOSTNAME")
 
     def get_course(self, course_id: int, user_id: int):
-        url = "https://ubademy-14-prod.herokuapp.com/courses/exams"
+        url = f"{self.host}courses/exams"
         query_params = f"?user_id={user_id}&course_id={course_id}"
         response = requests.get(url + query_params)
         response.raise_for_status()
