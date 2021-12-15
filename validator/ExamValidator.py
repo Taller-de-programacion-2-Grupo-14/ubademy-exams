b18d78005d70d16d80edb509eb7310d6c5fdf10c
violeta <viperez@fi.uba.ar>
2021-12-14 22:23:18 -0300

    exam validator with courses endpoint

--- validator/ExamValidator.py
+++ validator/ExamValidator.py
@@ -1,25 +1,29 @@
+from external.courses import Courses
+
+
 class ExamValidator:
     def __init__(self, database):
         self.db = database
+        self.courses = Courses()
 
     def is_course_creator(self, course_id, user_id):
-        is_creator = True
-        return is_creator
+        course = self.courses.get_course(course_id, user_id)
+        return course["can_edit"]
 
-    def exams_limit_reached(self, course_id):
-        return False
+    def exams_limit_reached(self, number, course_id, user_id):
+        course = self.courses.get_course(course_id, user_id)
+        og_quantity = course["exams"]
+        return og_quantity == number
 
     def is_student(self, course_id, user_id):
-        return True
+        course = self.courses.get_course(course_id, user_id)
+        return course["is_suscribed"]
 
     def is_course_collaborator(self, course_id, user_id):
-        return True
+        course = self.courses.get_course(course_id, user_id)
+        return course["can_collaborate"]
 
     def is_grader(self, course_id, user_id):
         collab = self.is_course_collaborator(course_id, user_id)
         creator = self.is_course_creator(course_id, user_id)
         return collab or creator
-
-    #def resolution_exists(self, id_exam, user_id):
-    #    resolutions = self.db.get_exam_resoltions(id_exam)
-    #    return user_id in resolutions
