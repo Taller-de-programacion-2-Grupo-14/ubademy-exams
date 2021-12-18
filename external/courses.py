fc0584b1005a3e878e83e409f7929a5ee1ea4478
violeta <viperez@fi.uba.ar>
2021-12-18 16:20:51 -0300

    update external

--- external/courses.py
+++ external/courses.py
@@ -7,8 +7,11 @@ class Courses:
         self.host = os.environ.get("COURSES_HOSTNAME")
 
     def get_course(self, course_id: int, user_id: int):
-        url = f"{self.host}courses/exams"
-        query_params = f"?user_id={user_id}&course_id={course_id}"
-        response = requests.get(url + query_params)
+        url = f"{self.host}courses/{course_id}/view"
+        response = requests.request(
+            method="get",
+            url=url,
+            data={"user_id": user_id}
+        )
         response.raise_for_status()
         return response.json()
